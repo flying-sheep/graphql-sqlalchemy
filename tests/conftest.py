402d496dab63c09fe88a72f6e9ab1aa89db7e19e
Phil Schaf <flying-sheep@web.de>
2023-10-22 18:21:38 +0200

    test types

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
+from collections.abc import Generator
+
 import pytest
 from sqlalchemy import Engine, create_engine
 from sqlalchemy.orm import Session, scoped_session, sessionmaker
 
 
 @pytest.fixture(scope="session")
-def db_engine() -> Engine:
+def db_engine() -> Generator[Engine, None, None]:
     engine = create_engine("sqlite+pysqlite:///:memory:", echo=False)
 
     yield engine
@@ -15,14 +17,14 @@ def db_engine() -> Engine:
 
 
 @pytest.fixture(scope="session")
-def db_session_factory(db_engine: Engine) -> scoped_session:
+def db_session_factory(db_engine: Engine) -> scoped_session[Session]:
     """returns a SQLAlchemy scoped session factory"""
     return scoped_session(sessionmaker(bind=db_engine))
 
 
 @pytest.fixture()
-def db_session(db_session_factory: scoped_session) -> Session:
-    """yields a SQLAlchemy connection which is rollbacked after the test"""
+def db_session(db_session_factory: scoped_session[Session]) -> Generator[Session, None, None]:
+    """yields a SQLAlchemy session which is rollbacked after the test"""
     session = db_session_factory()
 
     yield session
