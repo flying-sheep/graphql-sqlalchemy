ee1d980c187a3dfb638f58f3e1eb811af8b6f06d
Phil Schaf <flying-sheep@web.de>
2023-11-08 15:32:50 +0100

    simplify type test

--- tests/test_types.py
+++ tests/test_types.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any
+from typing import TYPE_CHECKING, Any
 
 import pytest
 from graphql import (
@@ -13,18 +13,23 @@ from graphql import (
     is_equal_type,
 )
 from graphql_sqlalchemy.graphql_types import get_graphql_type_from_column
-from sqlalchemy import ARRAY, Boolean, Column, Integer, String
+from sqlalchemy import ARRAY, Boolean, Float, Integer, String
+
+if TYPE_CHECKING:
+    from sqlalchemy.sql.type_api import TypeEngine
 
 
 @pytest.mark.parametrize(
-    ("column", "expected"),
+    ("sqla_type", "expected"),
     [
-        pytest.param(Column("int", Integer), GraphQLInt, id="int"),
-        pytest.param(Column("float", Integer), GraphQLInt, id="float"),
-        pytest.param(Column("bool", Boolean), GraphQLBoolean, id="bool"),
-        pytest.param(Column("str", String), GraphQLString, id="str"),
-        pytest.param(Column("arr", ARRAY(String)), GraphQLList(GraphQLNonNull(GraphQLString)), id="arr"),
+        pytest.param(Integer, GraphQLInt, id="int"),
+        pytest.param(Float, GraphQLInt, id="float"),
+        pytest.param(Boolean, GraphQLBoolean, id="bool"),
+        pytest.param(String, GraphQLString, id="str"),
+        pytest.param(ARRAY(String), GraphQLList(GraphQLNonNull(GraphQLString)), id="arr"),
     ],
 )
-def test_get_graphql_type_from_column(column: Column[Any], expected: GraphQLScalarType | GraphQLList[Any]) -> None:
-    assert is_equal_type(get_graphql_type_from_column(column.type), expected)
+def test_get_graphql_type_from_column(
+    sqla_type: TypeEngine[Any], expected: GraphQLScalarType | GraphQLList[Any]
+) -> None:
+    assert is_equal_type(get_graphql_type_from_column(sqla_type), expected)
