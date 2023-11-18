60df581560217468765bfbef23e1404b56eea04d
Philipp A. <flying-sheep@web.de>
2023-11-18 14:11:28 +0100

    list comp

--- src/graphql_sqlalchemy/testing.py
+++ src/graphql_sqlalchemy/testing.py
@@ -2,20 +2,22 @@ from __future__ import annotations
 
 from typing import Any
 
-from graphql import GraphQLEnumType, GraphQLEnumValueMap, GraphQLNonNull, is_equal_type
+from graphql import GraphQLEnumType, GraphQLEnumValueMap, GraphQLList, GraphQLNonNull, GraphQLType, is_equal_type
 
 
-def assert_equal_gql_type(a: Any, b: Any) -> None:
+def assert_equal_gql_type(a: GraphQLType, b: GraphQLType) -> None:
     if is_equal_type(a, b):
         return
-    if isinstance(a, GraphQLNonNull) and isinstance(b, GraphQLNonNull):
+    if (isinstance(a, GraphQLNonNull) and isinstance(b, GraphQLNonNull)) or (
+        isinstance(a, GraphQLList) and isinstance(b, GraphQLList)
+    ):
         assert_equal_gql_type(a.of_type, b.of_type)
         return
-    assert isinstance(a, GraphQLEnumType), a
-    assert isinstance(b, GraphQLEnumType), b
-    assert a.name == b.name
-    assert mk_comparable_values(a.values) == mk_comparable_values(b.values)
+    if isinstance(a, GraphQLEnumType) and isinstance(b, GraphQLEnumType):
+        assert a.name == b.name
+        assert mk_comparable_values(a.values) == mk_comparable_values(b.values)
+    raise NotImplementedError(f"Cannot compare {a} and {b}")
 
 
-def mk_comparable_values(values: GraphQLEnumValueMap) -> Any:
+def mk_comparable_values(values: GraphQLEnumValueMap) -> dict[str, Any]:
     return {k: v.value for k, v in values.items()}
